d3d6df42656dbc09ebc335a8f01af1b39c38053b
foshesss <foshessss@gmail.com>
2022-03-14 13:50:37 -0400

    working in class-- some pseudocode

--- Monster.lua
+++ Monster.lua
@@ -1,3 +1,4 @@
+local Encyclopedia = _G.Encyclopedia
 --[[
 [VESTERIA]
 -[Monster.lua]---------------
@@ -24,38 +25,56 @@ local Monster = {
 }
 Monster.__index = Monster
 
------ Utils -----
+----- Services -----
 local EntityService = require("EntityService")
+local ServerStorage = game:GetService("ServerStorage")
+
+----- Utils -----
 
 ----- Global methods -----
-function Monster.new(id, location, spawnRegionCollection, spawnRegion, other_info, init_callback)
+function Monster.new(id, location, spawnRegionCollection, spawnRegion, other_info, post_init_callback)
     local monster = EntityService.GetEntityByID(id)
     local stats = monster.Stats or {} -- replace with default stats
-    local client_render = monster.File.Model:Clone() -- TODO (?)
+    local client_render = Encyclopedia.TraversePath(ServerStorage, monster.File.Model)
+
+    -- setup hitbox
+    local hitbox = client_render.Hitbox
+    hitbox.Name = monster.id
+    hitbox:SetAttribute("ID", id)
 
     local self = setmetatable({
         id = id,
 
         client_render = client_render,
+        hitbox = hitbox,
 
-        health = stats.MaxHealth, 
+        health = stats.MaxHealth,
         -- TBA, still gotta read how they manage non-read only stats, like health
 
         _stats = stats -- read-only copy
-
-
-
-
-
-
-
     }, Monster)
 
     -- in the original vesteria, they seem to make
     -- some state machine, but it looks extremely confusing
 
-    
+    if post_init_callback ~= nil then
+        post_init_callback(self)
+        --[[
+        Example: post_init_callback function:
 
+            function(monster)
+                self.manifest:GetAttributeChangedSignal("Color"):Connect(function()
+                    -[
+                    set monster's color to something else
+                    -]
+                end)
+            end
+        --]]
+    end
+
+
+    -- fire event that monster is spawning
+    -- add this to some monster collection
 
     return self
 end
